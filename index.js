24be436b2dd14aaac804cb2d467fc4acf1104b71
jonathas.marques102030 <jonathascraft102030gtav@gmail.com>
2024-03-05 13:02:49 -0300

    alteracoes para o deploy da atividade

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
-(async () =>{
-    const dataBase = require("./src/connection/db")
-    const Produto = require("./src/models/produto")
-    const Usuario = require("./src/models/usuario")
-    await dataBase.sync()
-})();
-
-require("./src/connection/db")
 const express = require("express");
 const bodyParser = require('body-parser');
 const cors = require("cors");
 const usuarioRouter = require("./src/routes/usuario.routes");
 const produtoRouter = require("./src/routes/produto.routes");
-const app = express()
-const port = 3020 
+const app = express();
+
+const port = process.env.PORT || 3020; // Use a variável de ambiente PORT se disponível, caso contrário, use a porta 3020
 
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-
-app.use(cors())
-app.use(express.json())
-app.use(usuarioRouter)
-app.use(produtoRouter)
+app.use(cors());
+app.use(express.json());
+app.use(usuarioRouter);
+app.use(produtoRouter);
 
 app.listen(port, () => {
-   console.log("Servidor rodando na porta: " + port) 
+   console.log("Servidor rodando na porta: " + port);
 });
